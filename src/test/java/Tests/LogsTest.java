8380543b4a9253d0a9f38522d55bc77692a42109
az3l1t <eternity_cr9p@mail.ru>
2024-02-17 21:46:20 +0300

    Added the logger system

--- src/test/java/Tests/LogsTest.java
+++ src/test/java/Tests/LogsTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import java.util.logging.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,6 +25,7 @@ public class LogsTest {
     private static WebDriver driver;
     private static LoginPage loginPage;
     private static InformationPage informationPage;
+    private static Logger logger = Logger.getLogger(LogsTest.class.getName());
 
     @BeforeClass
     public static void BeforeAll() {
@@ -34,13 +36,14 @@ public class LogsTest {
         //profilePage = new ProfilePage(driver);
         driver.manage().window().maximize();
         driver.get("https://passport.yandex.ru/auth");
-
+        logger.info("Переход на страницу авторизации");
     }
 
     @Test
     public void LogginTest() throws InterruptedException{
         loginPage.SendKeysLogin("onelive32111@mail.ru");
         loginPage.ClickOnLoginButton();
+        logger.info("Введен логин");
         /*
          * Ожидание появления ввода
          * пароля
@@ -52,6 +55,7 @@ public class LogsTest {
          */
         loginPage.SendKeysPassword("Chemege1.");
         loginPage.ClickOnLoginButton();
+        logger.info("Введен пароль");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div/nav/ul/li[2]/a")));
 
@@ -59,12 +63,8 @@ public class LogsTest {
         WebDriverWait wait_second = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait_second.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/main/div/section[3]/div/div[3]/a/div[2]/div[1]/div/div/span")));
         informationPage.tapAllDocsElement();
-
         informationPage.tapPassport();
-
-
-        String nameOfRoot = informationPage.getName();
-        System.out.println(nameOfRoot);
+        logger.info("Перейден на паспорт-информацию");
 
         //driver.switchTo().frame(informationPage.getFrame());
         /*
@@ -74,20 +74,21 @@ public class LogsTest {
         WebDriverWait wait_third = new WebDriverWait(driver, Duration.ofSeconds(4));
         wait_third.until(ExpectedConditions.visibilityOfElementLocated(By.xpath
         ("//div[@class='Slot_root__jYlNI Slot_content__XYDYF alignment-center_root__ndulA color-inherit_root__OQmPQ Slot_direction_vertical__I3MEt FieldItem_content__adUIk']")));
-        // String passportNumber = informationPage.getPassportNumber();
-        // String dateOfBirth = informationPage.getDateOfBirth();
-        // String words[] = passportNumber.split(" ");
-        // System.out.println(Arrays.toString(words));
-        // System.out.println(passportNumber);
-        // System.out.println(dateOfBirth);
         System.out.println(informationPage.setArray(0));
+        logger.info("Данные получены!");
+        System.out.println(informationPage.setArray(1));
+        logger.info("Данные получены!");
+        System.out.println(informationPage.setArray(2));
+        logger.info("Данные получены!");
         System.out.println(informationPage.setArray(4));
+        logger.info("Данные получены!");
         System.out.println(informationPage.setArray(8));
+        logger.info("Данные получены!");
         System.out.println(informationPage.setArray(9));
+        logger.info("Данные получены!");
         /*
          * Добавить логгирование и try-catch
          */
-        // 
 
     }
 
